4b749a0cf3d66c09ce77672e52f7cd84d6a55c77
ArschieBald <38244115+ArschieBald@users.noreply.github.com>
2018-04-11 12:38:29 +0300

    Add files via upload

---
+++ 5_Smallest_multiple.py
@@ -0,0 +1,6 @@
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+#
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+print(5 * 7 * 9 * 11 * 13 * 16 * 17 * 19)
+# Bölünme kurallarını uyguladım.Örneğin bir sayı 16'ya bölünüyorsa bu aynı zamanda 2, 4, 8'e de bölünüyor demektir.
